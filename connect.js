8b56488bc82c17906e9de32eefd6031da06bb293
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-21 15:16:40 +0000

    Updating

--- connect.js
+++ connect.js
@@ -5,7 +5,7 @@ var http = require('http');
 //setup our port
 var port = process.env.PORT || 1337;
 // Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://<alexander>:<password>@ds050869.mlab.com:50869/mongodatabase1401061';
+var url = 'mongodb://alexander:password@ds050869.mlab.com:50869/mongodatabase1401061';
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
 var MongoClient = mongodb.MongoClient;
 
