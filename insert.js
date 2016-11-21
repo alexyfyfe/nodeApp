5723e354d6fc3194561b351ee6ec81313ec824c8
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-21 15:30:29 +0000

    Updating

--- insert.js
+++ insert.js
@@ -18,18 +18,20 @@ http.createServer(function(request, response) {
     var user2 = {name: 'modulus user', age: 22, roles: ['user']};
     var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
     // Insert some users
-    collection.insert([user1, user2, user3], function (err, result) {
-        if (err) {
-            response.write('Insert failed ' + err + "\n");
-        } else {
-            console.log(result);
-            response.write('Inserted ' + result.insertedCount +' documents ok. +"\n"');
-        }
-        //Close connection
-        db.close();
-        response.end('Finished, Connection closed \n');
-        //remove any other db.close or response.end statement below this line
+    MongoClient.connect(url, function (err, db) {
+        collection.insert([user1, user2, user3], function (err, result) {
+            if (err) {
+                response.write('Insert failed ' + err + "\n");
+            } else {
+                console.log(result);
+                response.write('Inserted ' + result.insertedCount + ' documents ok. +"\n"');
+            }
+            //Close connection
+            db.close();
+            response.end('Finished, Connection closed \n');
+            //remove any other db.close or response.end statement below this line
+        });
     });
-    
+        
 
 }).listen(port);
