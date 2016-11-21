fca98672603f1455025018c85d35634ef11a5316
alexyfyfe <alexyfyfe@hotmail.com>
2016-11-21 16:01:39 +0000

    Updating

--- retrieve.js
+++ retrieve.js
@@ -14,38 +14,22 @@ http.createServer(function(request, response) {
     response.write('Connecting \n');
     // Use connect method to connect to the Server
     MongoClient.connect(url, function (err, db) {
-        response.write('Connection Made \n');
-        if (err) {
-            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
-            //Error so close connection
-            db.close();
-        } else {
-            //HURRAY!! We are connected. :)
-            response.write('Connection established to' + url +"\n");
-            // Get the documents collection
-            var collection = db.collection('users');
-            //We have a cursor now with our find criteria
-            var results = collection.find({name: 'modulus user'});
-            //Lets iterate on the result
-            results.each(function (err, result) {
-                //if the result is null, there are no more results, it’s ok to close everything
-                if (result == null) {
-                    response.end('Completed');
-                    db.close();
-                }
-                if (err) {
-                    response.write(err);
-                } else {
-                    response.write('Fetched: ' + result.name + " : " + result.age + " : " + result.roles.toString() +'\n');
-                }
-            });
-
-            // do some work here with the database.
-
-            //Done Close connection
-            db.close();
-        }
-        response.end('Finished, Connection closed \n');
+        // Get the documents collection
+        var collection = db.collection('users');
+        //We have a cursor now with our find criteria
+        var results = collection.find({name: 'modulus user'});
+        //Lets iterate on the result
+        results.each(function (err, result) {
+            //if the result is null, there are no more results, it’s ok to close everything
+            if (result == null) {
+                response.end('Completed');
+                db.close();
+            }
+            if (err) {
+                response.write(err);
+            } else {
+                response.write('Fetched: ' + result.name + " : " + result.age + " : " + result.roles.toString() + '\n');
+            }
+        });
     });
-
-}).listen(port);
+    }).listen(port);
